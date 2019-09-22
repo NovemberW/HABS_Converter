1368884595cac166d184571c2a5533f0d3280ca4
novemberW <nicholas_wolf@gmx.de>
2019-09-22 18:37:16 +0200

    Call changed due to testing

--- src/core/Starter.java
+++ src/core/Starter.java
@@ -5,8 +5,7 @@ public class Starter {
 
 	
 	public static void main(String[] args) {
-		System.out.println("Dummy");
-		Flywheel fw = new Flywheel();
+		Flywheel2 fw = new Flywheel2();
 		String[] args2 = new String[]{"starter","-f","./exampleFiles/BouncingBall.abs"};
 		fw.work(args2);
 //		fw.extract();
