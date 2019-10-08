ace584561b60a37b888c45d5a5627e7c7fd9e8a5
novemberW <nicholas_wolf@gmx.de>
2019-10-08 13:39:57 +0200

    Create AwaitGroup class

---
+++ src/info/AwaitGroups.java
@@ -0,0 +1,33 @@
+package info;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class AwaitGroups {
+	
+	private String await;
+	
+	private java.util.List<String> lines = new LinkedList<String>();
+	
+	public AwaitGroups(String await, List<String> lines) {
+		this.await = await;
+		this.lines = lines;
+	}	
+	
+	public String getAwait() {
+		return await;
+	}
+
+	public void setAwait(String await) {
+		this.await = await;
+	}
+
+	public java.util.List<String> getLines() {
+		return lines;
+	}
+
+	public void setLines(java.util.List<String> lines) {
+		this.lines = lines;
+	}
+
+}
