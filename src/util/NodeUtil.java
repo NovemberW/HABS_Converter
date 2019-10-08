df90e0eda83ed49bd16003817cb8b2774331916f
novemberW <nicholas_wolf@gmx.de>
2019-10-08 13:13:37 +0200

    Add AwaitStatement finder

--- src/util/NodeUtil.java
+++ src/util/NodeUtil.java
@@ -3,6 +3,7 @@ package util;
 import java.util.Iterator;
 
 import abs.frontend.ast.ASTNode;
+import abs.frontend.ast.AwaitStmt;
 import abs.frontend.ast.DifferentialExp;
 import abs.frontend.ast.PhysicalImpl;
 
@@ -21,6 +22,23 @@ public class NodeUtil {
 		ASTNode<ASTNode> akku = null;
 		while (it.hasNext() && akku == null) {
 			akku = recursiveFind_PhysicalImpl(it.next());
+		}
+		return akku;
+	}
+	
+	@SuppressWarnings({ "rawtypes", "unchecked" })
+	public static ASTNode<ASTNode> recursiveFind_AwaitStmt(ASTNode<ASTNode> node) {
+		if (node == null)
+			return null;
+		if (node instanceof AwaitStmt) {
+			return node;
+		}
+
+		Iterator<ASTNode> it = node.astChildIterator();
+
+		ASTNode<ASTNode> akku = null;
+		while (it.hasNext() && akku == null) {
+			akku = recursiveFind_AwaitStmt(it.next());
 		}
 		return akku;
 	}
