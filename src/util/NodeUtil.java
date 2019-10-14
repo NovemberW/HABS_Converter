ca257404216ea15d1f93724e19bdddc44230fecc
novemberW <nicholas_wolf@gmx.de>
2019-10-14 16:19:57 +0200

    Add: Functions for search of ClassDecl and MethodImpl

--- src/util/NodeUtil.java
+++ src/util/NodeUtil.java
@@ -4,10 +4,44 @@ import java.util.Iterator;
 
 import abs.frontend.ast.ASTNode;
 import abs.frontend.ast.AwaitStmt;
+import abs.frontend.ast.ClassDecl;
 import abs.frontend.ast.DifferentialExp;
+import abs.frontend.ast.MethodImpl;
 import abs.frontend.ast.PhysicalImpl;
 
 public class NodeUtil {
+	@SuppressWarnings({ "rawtypes", "unchecked" })
+	public static void  recursiveFind_ClassDecl(ASTNode<ASTNode> node, java.util.List<ASTNode<ASTNode>> akku ) {
+		if (node == null)
+			return;
+		if (node instanceof ClassDecl) {
+			akku.add(node);
+			return;
+		}
+
+		Iterator<ASTNode> it = node.astChildIterator();
+
+		ASTNode<ASTNode> akku2 = null;
+		while (it.hasNext() && akku2 == null) {
+			recursiveFind_ClassDecl(it.next(),akku);
+		}
+	}
+	
+	@SuppressWarnings({ "rawtypes", "unchecked" })
+	public static void  recursiveFind_MethodImpl(ASTNode<ASTNode> node, java.util.List<ASTNode<ASTNode>> akku ) {
+		if (node == null)
+			return;
+		if (node instanceof MethodImpl) {
+			akku.add(node);
+		}
+
+		Iterator<ASTNode> it = node.astChildIterator();
+
+		ASTNode<ASTNode> akku2 = null;
+		while (it.hasNext() && akku2 == null) {
+			recursiveFind_MethodImpl(it.next(),akku);
+		}
+	}
 
 	@SuppressWarnings({ "rawtypes", "unchecked" })
 	public static ASTNode<ASTNode> recursiveFind_PhysicalImpl(ASTNode<ASTNode> node) {
