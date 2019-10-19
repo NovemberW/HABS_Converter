f8ec4cd5d27f45687a158bf7dcd5555f280614b8
novemberW <nicholas_wolf@gmx.de>
2019-10-19 16:23:02 +0200

    Remove: Old structure of converting all into one state

--- src/info/ContentModel.java
+++ src/info/ContentModel.java
@@ -23,23 +23,19 @@ import util.StringTools;
 
 public class ContentModel {
 
-	private java.util.List<ContinousVariable> continousVariables;
 
 	private java.util.List<FlagInterface> availableFlags;
 
-	private java.util.List<AwaitGroups> awaitGroups;
 	
 	private java.util.List<ClassContent> classes;
 
 	public ContentModel(java.util.List<FlagInterface> availableFlags) {
-		continousVariables = new LinkedList<ContinousVariable>();
 		this.availableFlags = availableFlags;
-		this.awaitGroups = new LinkedList<AwaitGroups>();
 		this.classes = new LinkedList<ClassContent>();
 	}
 
 	@SuppressWarnings({ "unchecked", "rawtypes" })
-	public void extractInfo(String filename) throws FileNotFoundException, IOException {
+	public void extract(String filename) throws FileNotFoundException, IOException {
 
 		String fileContent = "";
 		for (FlagInterface flag : availableFlags)
@@ -55,18 +51,8 @@ public class ContentModel {
 
 		Iterator<ASTNode> it = model.astChildIterator();
 
-		ASTNode<ASTNode> root = it.next();
+		ASTNode<ASTNode> root = it.next();//Base of ast
 
-		ASTNode<ASTNode> physicalBlock = NodeUtil.recursiveFind_PhysicalImpl(root);
-
-		Iterator<FieldDecl> fieldIterator = ((PhysicalImpl) physicalBlock).getFieldList().iterator();
-
-		while (fieldIterator.hasNext()) {
-			FieldDecl fD = fieldIterator.next();
-//			System.out.println("-> " + fD.getName());
-//			System.out.println("-> " + fD);
-			continousVariables.add(new ContinousVariable(fD.getName(), fileContent));
-		}
 
 		java.util.List<String> segment = new LinkedList<String>();
 
@@ -74,7 +60,6 @@ public class ContentModel {
 
 		extractFunctions(lines);
 		
-//		extractAwaits(segment, lines);
 		java.util.List<ASTNode<ASTNode>> list = new LinkedList<ASTNode<ASTNode>>();
 		NodeUtil.recursiveFind_ClassDecl(root, list);
 		
@@ -88,43 +73,12 @@ public class ContentModel {
 		
 	}
 
-	private void extractAwaits(java.util.List<String> segment, String[] lines) {
-		for (int i = 0; i < lines.length; i++)
-			lines[i] = lines[i].replaceAll(" ", " ").trim();
-
-		for (int i = 0; i < lines.length; i++) {
-			if (lines[i].contains("await")) {
-				String awaitStatement = lines[i];
-				i++;// Skipping the line with await
-				boolean end = false;
-				int openBrackets = -1;
-				for (; i < lines.length && !end; i++) {
-					String line = lines[i];
-					for(char c : line.toCharArray()) {
-						if(c == '{') 
-							openBrackets++;
-						if(c == '}')
-							openBrackets--;
-					}
-//					System.out.println(openBrackets);
-					if (lines[i].contains("!") || openBrackets == 0) {
-						awaitGroups.add(new AwaitGroups(awaitStatement, segment));
-						segment = new LinkedList<String>();
-						end = true;
-					}
-					segment.add(lines[i]);
-				}
-			}
-		}
-	}
 
 	public static void recursivePrinter(ASTNode node) {
 		if (node == null)
 			return;
 		System.out.println(node.value);
 		Iterator<ASTNode> it = node.astChildIterator();
-//		if(!it.hasNext())
-//			System.out.println(node.value);
 
 		while (it.hasNext()) {
 			recursivePrinter(it.next());
@@ -135,29 +89,11 @@ public class ContentModel {
 	public String toString() {
 		StringBuffer sb = new StringBuffer();
 
-		for (ContinousVariable cV : continousVariables) {
-			sb.append(cV.toString());
-			sb.append(System.lineSeparator());
-		}
-
-		sb.append("\n");
-
-		for (AwaitGroups aG : awaitGroups) {
-			sb.append(aG.toString());
-			sb.append("\n");
-		}
+		//TODO add new Substructure when implemented
 
 		return sb.toString();
 	}
 
-	public java.util.List<AwaitGroups> getAwaitGroups() {
-		return awaitGroups;
-	}
-
-	public void setAwaitGroups(java.util.List<AwaitGroups> awaitGroups) {
-		this.awaitGroups = awaitGroups;
-	}
-	
 	public static ASTNode<ASTNode> recursiveTraverserFind(@SuppressWarnings("rawtypes") ASTNode node, String startText) {
 		if (node == null)
 			return null;
