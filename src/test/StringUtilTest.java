3da2959c046e278a11321b063dadc23bc17552fe
novemberW <nicholas_wolf@gmx.de>
2019-11-21 09:14:43 +0100

    Add: Remaining tests for StringTool

--- src/test/StringUtilTest.java
+++ src/test/StringUtilTest.java
@@ -23,7 +23,10 @@ public class StringUtilTest {
 
 	@Test
 	public void convertToTransitionTest() {
-
+		String[][] testVector = {{"d = 42;","d := 42"},{"this.x = 13;","x := 13"}};
+		
+		for(String[] v : testVector)
+			assertEquals(v[1], StringTools.convertToTransition(v[0]));
 	}
 
 	@Test
@@ -31,7 +34,7 @@ public class StringUtilTest {
 
 		String equation = "3 <= pi";
 		String solution = "3 > pi";
-		
+
 		String result = StringTools.invertEquation(equation);
 		assertEquals(result, solution);
 	}
@@ -46,11 +49,30 @@ public class StringUtilTest {
 	}
 
 	@Test
-	public void getWithPrettyPrintTest() {
+	public void containCaseSensitivetTest() {
+		String element = "Dummy";
+		String[] toTest = { "A", "b", "heslfsjfd", element, element.toLowerCase() };
+		String[] toTest2 = { "A", "b", "heslfsjfd", element.toLowerCase() };
+
+		boolean answer = StringTools.containsCaseSensitive(toTest, element);
+		assertEquals(true, answer);
+		
+		boolean answer2 = StringTools.containsCaseSensitive(toTest2, element);
+		assertEquals(false, answer2);
+		
 	}
 
 	@Test
-	public void removeSemicolonTest() {
-	}
+	public void containNotCaseSensitivetTest() {
+		String element = "Dummy";
+		String[] toTest = { "A", "b", "heslfsjfd", element, element.toLowerCase() };
+		String[] toTest2 = { "A", "b", "heslfsjfd", element.toLowerCase() };
 
+		boolean answer = StringTools.containsNotCaseSensitive(toTest, element);
+		assertEquals(true, answer);
+		
+		boolean answer2 = StringTools.containsNotCaseSensitive(toTest2, element);
+		assertEquals(true, answer2);
+
+	}
 }
