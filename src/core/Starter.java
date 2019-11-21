a6923b712bc73642711bf66a4453ed510ebecc1c
novemberW <nicholas_wolf@gmx.de>
2019-11-21 09:53:53 +0100

    Remove old worker (Flywheel) and remove now unused imports

--- src/core/Starter.java
+++ src/core/Starter.java
@@ -1,24 +1,11 @@
 package core;
 
-import abs.frontend.ast.DifferentialExp;
-import util.StringTools;
-
 public class Starter {
 
 	public static void main(String[] args) {
 		Flywheel2 fw = new Flywheel2();
-//		String[] args2 = new String[]{"starter","-f","./exampleFiles/BouncingBall.abs"};
 
-//		String[] args2 = new String[] { "starter", "-f", "./exampleFiles/SimpleTank.abs","-s","test"};
 		String[] args2 = new String[] { "starter", "-f", "./exampleFiles/SimpleBouncingBall.abs","-s","secondAuto"};
-//		String[] args2 = new String[] { "starter", "-f", "./exampleFiles/Verkehrssimulation.abs" };
 		fw.work(args2);
-
-//		Flywheel f = new Flywheel();
-
-//		args2 = new String[]{"starter","-print","-f","./exampleFiles/BouncingBall.abs"};
-//		f.work(args2);
-//		f.extract();
-
 	}
 }
