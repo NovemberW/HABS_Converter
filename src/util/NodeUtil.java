905c9f1c7982d145897ba446b8542e94ecba6057
novemberW <nicholas_wolf@gmx.de>
2019-11-25 08:37:44 +0100

    Add: Javadoc for NodeUtil

--- src/util/NodeUtil.java
+++ src/util/NodeUtil.java
@@ -9,8 +9,17 @@ import abs.frontend.ast.ClassDecl;
 import abs.frontend.ast.DifferentialExp;
 import abs.frontend.ast.MethodImpl;
 import abs.frontend.ast.PhysicalImpl;
-
+/**
+ * Collector Class for static functions to ease work with ASTNodes
+ * @author nicholas
+ *
+ */
 public class NodeUtil {
+	/* Loads the given list akku with all instances of ClassDecl (@see absfrontend.jar)
+	 * that are present in the subtree starting with given ASTNode node
+	 * @param node - the root of the subtree to be searched through.
+	 * @param akku - Will hold all such nodes
+	 */
 	@SuppressWarnings({ "rawtypes", "unchecked" })
 	public static void  recursiveFind_ClassDecl(ASTNode<ASTNode> node, java.util.List<ASTNode<ASTNode>> akku ) {
 		if (node == null)
@@ -28,6 +37,11 @@ public class NodeUtil {
 		}
 	}
 	
+	/* Loads the given list akku with all instances of MethodlImpl (@see absfrontend.jar)
+	 * that are present in the subtree starting with given ASTNode node
+	 * @param node - the root of the subtree to be searched through.
+	 * @param akku - Will hold all such nodes
+	 */
 	@SuppressWarnings({ "rawtypes", "unchecked" })
 	public static void  recursiveFind_MethodImpl(ASTNode<ASTNode> node, java.util.List<ASTNode<ASTNode>> akku ) {
 		if (node == null)
@@ -44,6 +58,11 @@ public class NodeUtil {
 		}
 	}
 	
+	/* Returns a list of all instances of PhysicalImpl (@see absfrontend.jar)
+	 * that are present in the subtree starting with given ASTNode classsDecl
+	 * @param classDecl - the root of the subtree to be searched through.
+	 * @return a list of all such nodes
+	 */
 	public static java.util.List<ASTNode<ASTNode>> getAllPhysicalImpl(ASTNode<ASTNode> classDecl){
 		java.util.List<ASTNode<ASTNode>> akku = new LinkedList<ASTNode<ASTNode>>();
 		
@@ -52,7 +71,10 @@ public class NodeUtil {
 		return akku;
 		
 	}
-
+	/* Recursive assembly of the list for getAllPhysicalImpl
+	 * @param node the current node
+	 * @param akku the list in its previous state
+	 */
 	private static java.util.List<ASTNode<ASTNode>> getAllPhysicalImplHelper(ASTNode<ASTNode> node,java.util.List<ASTNode<ASTNode>> akku){
 
 		if (node == null)
@@ -74,6 +96,11 @@ public class NodeUtil {
 		
 	}
 	
+	/* Returns first ASTNode of type PhysicalImpl that can
+	 * be found based on the given ASTNode node.
+	 * @param node- The root of the subtree to be searched through.
+	 * @return that node
+	 */
 	@SuppressWarnings({ "rawtypes", "unchecked" })
 	public static ASTNode<ASTNode> recursiveFind_PhysicalImpl(ASTNode<ASTNode> node) {
 		if (node == null)
@@ -84,10 +111,10 @@ public class NodeUtil {
 
 		Iterator<ASTNode> it = node.astChildIterator();
 
-		ASTNode<ASTNode> akku = null;
-		while (it.hasNext() && akku == null) {
-			akku = recursiveFind_PhysicalImpl(it.next());
+		ASTNode<ASTNode> temp = null;
+		while (it.hasNext() && temp == null) {
+			temp = recursiveFind_PhysicalImpl(it.next());
 		}
-		return akku;
+		return temp;
 	}
 }
